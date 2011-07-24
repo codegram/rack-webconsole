a2a6d33710ec11b72eee60576569f096ff0a5052
Josep M. Bach <josep.m.bach@gmail.com>
2011-07-24 17:12:49 +0200

    docs for Rack::Webconsole

--- lib/rack/webconsole.rb
+++ lib/rack/webconsole.rb
@@ -11,20 +11,39 @@ require 'rack/webconsole/railtie' if defined?(Rails)
 #   http://github.com/rack/rack
 #
 module Rack
+  # {Rack::Webconsole} is a Rack middleware that provides an interactive
+  # console à la Rails console, but for any kind of Rack application (Rails,
+  # Sinatra, Padrino...), accessible from your web application's front-end.
+  #
+  # For every request, it normally passes control to the {Assets} middleware,
+  # which injects needed JavaScript, CSS and HTML code for the console to work
+  # properly.
+  #
+  # It also exposes a special route used by the {Repl}, a Ruby evaluator which
+  # is responsible of keeping state between requests, remembering local
+  # variables and giving a true IRB-esque experience.
+  #
   class Webconsole
-
+    # Honor the Rack contract by saving the passed Rack application in an ivar.
+    #
+    # @param [Rack::Application] app the previous Rack application in the
+    #   middleware chain.
     def initialize(app)
       @app = app
     end
 
+    # Decides where to send the request. In case the path is `/webconsole`
+    # (e.g. when calling the {Repl} endpoint), pass the request onto the
+    # {Repl}. Otherwise, pass it onto the {Assets} middleware, which will
+    # inject the needed assets for the Webconsole to work.
+    #
+    # @param [Hash] env a Rack request environment.
     def call(env)
-      # Pass the chain onto the Repl if appropriate
       if env['PATH_INFO'] == '/webconsole'
         Repl.new(@app).call(env)
-      else # if not, pass it to the assets
+      else
         Assets.new(@app).call(env)
       end
     end
-
   end
 end
